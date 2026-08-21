0ae8fa600d4a6c3bd2be5af403f264188d0f17b0
COSMIC <961737902@qq.com>
2026-08-21 20:37:45 +0800

    feat(web): allow zooming out to min cell size 3 on map view

--- web/src/components/game/WorldCanvas.test.ts
+++ web/src/components/game/WorldCanvas.test.ts
@@ -48,7 +48,7 @@ describe('wheelZoomCell', () => {
   })
 
   it('clamps zoom to the supported cell range', () => {
-    expect(wheelZoomCell(6, 10_000, 0, 720)).toBe(6)
+    expect(wheelZoomCell(3, 10_000, 0, 720)).toBe(3)
     expect(wheelZoomCell(78, -10_000, 0, 720)).toBe(78)
   })
 })
